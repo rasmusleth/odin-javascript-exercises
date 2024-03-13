0fcc5a55ac69f120a92c3b7acf8438da3d622035
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-13 12:53:28 +0100

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (lowerBound, upperBound) {
+  if (lowerBound < 0 || upperBound < 0) return "ERROR";
+  if (!Number.isInteger(lowerBound) || !Number.isInteger(upperBound))
+    return "ERROR";
+  if (lowerBound > upperBound) {
+    let tmpValue = upperBound;
+    upperBound = lowerBound;
+    lowerBound = tmpValue;
+  }
 
+  let sum = 0;
+  while (lowerBound <= upperBound) {
+    sum += lowerBound;
+    lowerBound++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
