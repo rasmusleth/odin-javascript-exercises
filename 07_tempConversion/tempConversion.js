f83a4659ada3a00b98ca651826a025a3dea3ea29
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-13 13:38:48 +0100

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesFahreheit) {
+  let degreesCelsius = (degreesFahreheit - 32) * (5 / 9);
+  return parseFloat(degreesCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesCelsius) {
+  let degreesFahrenheit = degreesCelsius * (9 / 5) + 32;
+  return parseFloat(degreesFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
