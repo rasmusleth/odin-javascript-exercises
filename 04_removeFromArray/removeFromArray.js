9b96483d5ff074c5b24386a8c4efabc4219b8403
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-13 12:40:04 +0100

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...inputsToRemove) {
+  inputsToRemove.forEach((inputToRemove) => {
+    array = array.filter((item) => {
+      return item !== inputToRemove;
+    });
+  });
+  return array;
 };
 
 // Do not edit below this line
