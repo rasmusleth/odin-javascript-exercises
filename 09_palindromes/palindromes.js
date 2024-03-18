946a351ccabb33b5ba090ec2f7d2c168c50ddcde
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-18 12:59:47 +0100

    Solve exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyzæøå0123456789";
 
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => {
+      return alphanumerical.includes(character);
+    })
+    .join("");
+
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
