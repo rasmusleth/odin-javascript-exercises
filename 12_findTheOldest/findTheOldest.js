e580c48ff7bfa2f55faabe458264c28872c361b3
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-18 13:54:55 +0100

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  for (person of peopleArray) {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  }
 
+  let ageArray = peopleArray.map((person) => {
+    person["age"] = person.yearOfDeath - person.yearOfBirth;
+    return person;
+  });
+
+  ageArray.sort((a, b) => b.age - a.age);
+
+  return ageArray[0];
 };
 
 // Do not edit below this line
