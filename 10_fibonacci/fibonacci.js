68581a8289a2c051180f4a743b22d764ed0e6074
Rasmus Leth <rasmus@rasmusleth.dk>
2024-03-18 13:23:15 +0100

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (memberIndex) {
+  if (parseInt(memberIndex) === undefined)
+    return console.log("Error, must be a number");
+  if (memberIndex < 0) return "OOPS";
 
+  let fibonacciSequence = [0, 1];
+  let numbersToCalculate = memberIndex - 1;
+
+  for (let i = 0; i < numbersToCalculate; i++) {
+    fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[i + 1]);
+  }
+
+  return fibonacciSequence[memberIndex];
 };
 
 // Do not edit below this line
